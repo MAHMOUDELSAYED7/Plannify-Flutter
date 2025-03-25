4e40c529006572fa4ec33785f05bce6c2ccdd7f5
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2025-03-25 07:31:48 +0200

    add authentication and logging interceptors for network requests

---
+++ lib/core/network/interceptors/auth_interceptor.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+
+import '../../security/secure_storage_repository.dart';
+
+class AuthInterceptor extends Interceptor {
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    SecureStorageManager.getAuthToken()
+        .then((token) {
+          if (token != null) {
+            options.headers['Authorization'] = 'Bearer $token';
+          }
+          super.onRequest(options, handler);
+        })
+        .catchError((error) {
+          handler.reject(
+            DioException(
+              requestOptions: options,
+              type: DioExceptionType.badCertificate,
+              error: 'Failed to retrieve token: $error',
+            ),
+          );
+        });
+  }
+}
