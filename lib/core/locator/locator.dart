09ed46545c30c78f9a4e553b119da7ef1a0a6ed1
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2025-03-26 08:17:57 +0200

    add dependency injection for authentication components and secure storage management

--- lib/core/locator/locator.dart
+++ lib/core/locator/locator.dart
@@ -1,7 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+import '../../data/repositories/auth_repository_impl.dart';
+import '../../data/source/auth_remote_data_source.dart';
 
 import '../network/dio_client.dart';
+import '../security/secure_storage_repository.dart';
 
 final locator = GetIt.instance;
 
@@ -9,4 +14,18 @@ Future<void> setupLocator() async {
   locator.registerSingleton(Dio());
 
   locator.registerLazySingleton(() => DioClient(locator<Dio>()));
+  locator.registerLazySingleton(
+    () => AuthRemoteDataSourceImpl(dioClient: locator<DioClient>()),
+  );
+
+  locator.registerLazySingleton(() => SecureStorageManager());
+  locator.registerSingleton(InternetConnectionChecker.createInstance());
+
+  locator.registerLazySingleton(
+    () => AuthRepositoryImpl(
+      remoteDataSource: locator<AuthRemoteDataSourceImpl>(),
+      secureStorage: locator<SecureStorageManager>(),
+      connectionChecker: locator<InternetConnectionChecker>(),
+    ),
+  );
 }
--- lib/core/network/interceptors/auth_interceptor.dart
+++ lib/core/network/interceptors/auth_interceptor.dart
@@ -1,11 +1,13 @@
 import 'package:dio/dio.dart';
+import 'package:plannify/core/locator/locator.dart';
 
 import '../../security/secure_storage_repository.dart';
 
 class AuthInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    SecureStorageManager.getAuthToken()
+    locator<SecureStorageManager>()
+        .getAuthToken()
         .then((token) {
           if (token != null) {
             options.headers['Authorization'] = 'Bearer $token';
